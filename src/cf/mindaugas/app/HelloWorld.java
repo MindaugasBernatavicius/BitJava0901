6b22c3f1cc234ae525dcaa30f5fbf986eda946fa
MindaugasBernatavicius <darbas.minaugas@gmail.com>
2020-09-03 13:21:08 +0300

    Adding initial project files

---
+++ src/cf/mindaugas/app/HelloWorld.java
@@ -0,0 +1,8 @@
+package cf.mindaugas.app;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.print("Hello, World!");
+        System.out.print("Hello, World!");
+    }
+}
