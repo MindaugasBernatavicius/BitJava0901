1509ccdcfe7ce04d6e099e6bdba9e0f0d80f8a66
MindaugasBernatavicius <darbas.minaugas@gmail.com>
2020-09-08 15:16:11 +0300

    Adding arrays

--- src/cf/mindaugas/app/HelloWorld.java
+++ src/cf/mindaugas/app/HelloWorld.java
@@ -194,15 +194,15 @@ public class HelloWorld {
         // System.out.println("Username is: " + userName + " and the password is: " + password);  // Output user input
 
         // ... do-while
-        System.out.println("... do-while");
-        Scanner sc1 = new Scanner(System.in);
-        String userInput = null;
-        String psw = "poiuytr";
-        do {
-            System.out.print("Please enter password: ");
-            userInput = sc1.nextLine();
-        } while(!userInput.equals(psw)); // while userinput is not equal to password, do x
-        System.out.println("You are logged in!");
+        // System.out.println("... do-while");
+        // Scanner sc1 = new Scanner(System.in);
+        // String userInput = null;
+        // String psw = "poiuytr";
+        // do {
+        //     System.out.print("Please enter password: ");
+        //     userInput = sc1.nextLine();
+        // } while(!userInput.equals(psw)); // while userinput is not equal to password, do x
+        // System.out.println("You are logged in!");
 
         // ... for ciklo sudėtingesnė sintaksė
         int abc = 5;
@@ -219,5 +219,121 @@ public class HelloWorld {
         for(int u = 0; u < myAwesomeArray.length; u++){
             System.out.println(myAwesomeArray[u]);
         }
+
+        // ... How to reverse string in Java
+        System.out.println("... How to reverse string in Java");
+        String str = "alus";
+        String reversedString = new StringBuffer(str).reverse().toString();
+        System.out.println(reversedString);
+
+        String input = "alus"; // 4
+        // toCharArray() method to convert string into char[].
+        char[] charArr = input.toCharArray();
+        char[] resArr = new char[charArr.length];
+        // Store result in reverse order into the result byte[]
+        for (int idx = 0; idx < charArr.length; idx++)
+            resArr[idx] = charArr[charArr.length - idx - 1]; // pradžioje charArr.length - i - 1 = 3
+        System.out.println(resArr);
+
+
+        // ... break and continue
+        System.out.println("... break and continue");
+        int[] intArr = { 1, 15, 256, 5465, 121, 548, -55, 549, -5155156 };
+
+        // ... find first negative number
+        for (int k = 0; k < intArr.length; k++) {
+            if(intArr[k] < 0){
+                System.out.println(intArr[k]);
+                break;
+            }
+            System.out.println("Už if'o!");
+        }
+        System.out.println("Už ciklo!");
+
+        // ... find all
+        for (int k = 0; k < intArr.length; k++) {
+            if(intArr[k] < 0) System.out.println(intArr[k]);
+        }
+
+        // ... continue
+        System.out.println("... continue");
+        for (int k = 0; k < intArr.length; k++) {
+            if(intArr[k] < 0) {
+                System.out.println("Neigiamas: " + intArr[k]);
+                continue;
+            }
+            // jei intArr[k] < 0 == true, tai šios eilutės nevykdysime
+            System.out.println("Teigiamas: " + intArr[k]);
+        }
+
+        // ... arrays
+        System.out.println("... arrays");
+        int[] intArr2 = { 1, 15 }; // length == 2, tačiau nariai tik 0, 1
+        for (int k = 0; k < 5; k++) {
+            // java.lang.ArrayIndexOutOfBoundsException: Index 2 out of bounds for length 2
+            // System.out.println(intArr2[k]);
+        }
+        // k jau neegzistuoja
+
+        System.out.println("intArr2.length: " + intArr2.length);
+
+        // ... fixed size
+        int[] arr3 = new int[2]; // masyvo dydis yra aprašomas šioje vietoje: new int[2]
+        arr3[0] = 0;
+        arr3[1] = 1;
+        // arr3[2] = 2; // java.lang.ArrayIndexOutOfBoundsException: Index 2 out of bounds for length 2
+        arr3[1] = 55;
+        System.out.println("arr3[1]: " + arr3[1]);
+
+        // ... array initialization:
+        int[] intArr21 = new int[2];
+        intArr21[0] = 5; // adding a variable
+        int[] intArr22 = new int[]{ 1, 15 }; // + declaration of internal variables
+        int[] intArr23 = { 1, 15 }; // + declaration of internal variables
+
+        // String[] s = new String[5];
+        // s = new int[5];
+
+        float[] fArr = new float[5];
+        // double[] dArr = fArr; // negalime priskirti kito tipo masyvo
+
+        double d6 = 99.9;
+        fArr[0] = (float)d6; // galime su castingu priskirti
+
+        // ... Išvardinti kas antrą narį.
+        System.out.println("Išvardinti kas antrą narį.");
+        String[] myAwesomeArray2 = { "1", "2", "3", "4", "5" }; // masyvo deklaracija ir inicializacija
+        for (int k = 0; k < myAwesomeArray2.length; k += 2) {
+            System.out.println(myAwesomeArray2[k]);
+        }
+
+        // "0 , 1 , 2 ... , 4"
+        for (int k = 0; k < 5; k++) {
+            System.out.println(k); // 0 ... 4
+        }
+
+        // ... Išvardinti narius iš dviejų vienodo ilgio masyvų vienu ciklu.
+        System.out.println("Išvardinti narius iš dviejų vienodo ilgio masyvų vienu ciklu.");
+        String[] myAwesomeArray3 = { "1", "2", "3", "4", "5" };
+        String[] myAwesomeArray4 = { "5", "4", "3", "2", "1" };
+        System.out.println("myAwesomeArray3.length: " + myAwesomeArray3.length);
+        // for (int k = 0; k < myAwesomeArray3.length; k++) {
+        //     System.out.println(myAwesomeArray3[k] + " -- " + myAwesomeArray4[k]);
+        // }
+
+        for (int k = 0, k2 = 0; k < myAwesomeArray3.length && k2 < myAwesomeArray4.length; k++, k2++) {
+            System.out.println(myAwesomeArray3[k] + " -- " + myAwesomeArray4[k2]);
+        }
+
+        // ... filtravimas
+        int[] intArr3 = { 1, 4, 15, 256, 0, 5465, 121, 548, -55, 549, -5155156, 22, 12 };
+        for (int k = 0; k < intArr3.length; k++) {
+            // jei liekana dalinant iš dviejų (ne)lygi nuliui (a.k.a (ne)lyginis)
+            if (intArr3[k] % 2 != 0){
+                System.out.println("intArr3[" + k + "]: " + intArr3[k]);
+            }
+        }
+
+        // ... kaip iš jau turimo masyvo viršuje, pasigaminti antrą masyvą, kuris turėtų tik lyginius skaičius.
     }
 }
